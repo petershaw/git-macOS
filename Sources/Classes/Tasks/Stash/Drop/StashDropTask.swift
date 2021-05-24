08c8256323a50c8ec6b711066e4dbc29df9ed95a
Kris <kris@ausdertechnik.de>
2021-05-24 18:39:37 +0200

    Remove all Cocoa bindings and replaced it with Foundation

--- Sources/Classes/Tasks/Stash/Drop/StashDropTask.swift
+++ Sources/Classes/Tasks/Stash/Drop/StashDropTask.swift
@@ -15,7 +15,6 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-import Cocoa
 
 class StashDropTask: RepositoryTask, TaskRequirable {
     var name: String {
